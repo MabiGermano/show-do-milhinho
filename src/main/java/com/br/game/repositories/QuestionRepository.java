e9e85518e476b159f49572c8d09ae4195f63a6c1
MabiGermano <mabi.germano.e@gmail.com>
2021-09-08 16:07:20 -0300

    creating database logic

--- src/main/java/com/br/game/repositories/QuestionRepository.java
+++ src/main/java/com/br/game/repositories/QuestionRepository.java
@@ -3,13 +3,18 @@ package com.br.game.repositories;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 import com.br.game.database.DatabaseConnection;
 import com.br.game.models.Question;
 
 public class QuestionRepository {
 
-	public void list() {
+	public ArrayList<Question> list() {
+		ArrayList<Question> questionList = new ArrayList<>();
 		try {
 			DatabaseConnection database = DatabaseConnection.getInstance();
 
@@ -17,64 +22,77 @@ public class QuestionRepository {
 			Statement statement = database.getConnection().createStatement();
 			ResultSet dbResult = statement.executeQuery(query);
 			while (dbResult.next()) {
+				Question question = new Question();
+				question.setId(dbResult.getInt("id"));
+				question.setTitle(dbResult.getString("title"));
+				question.setRightAnswer(dbResult.getString("right_answer")); 
+				question.setTip(dbResult.getString("tip")); 
 
-				// Colocar um builder aqui???
-				int id = dbResult.getInt("id");
-				String title = dbResult.getString("title");
-				String wrongAnswers = dbResult.getString("wrong_answers");
-				String rightAnswers = dbResult.getString("right_answer");
+				String wrongAnswersFromDb = dbResult.getString("wrong_answers");
+				
+				Set<String> wrongAnswers = new HashSet<>();
+				Collections.addAll(wrongAnswers, wrongAnswersFromDb.split(","));
+				question.setWrongAnswers(wrongAnswers);
+				
+				questionList.add(question);
 
 			}
 
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-	}
-
-	public int count() {
-		int count = 0;
-		try {
-			DatabaseConnection database = DatabaseConnection.getInstance();
-
-			String query = "SELECT COUNT(*) FROM question";
-			Statement statement = database.getConnection().createStatement();
-			ResultSet dbResult = statement.executeQuery(query);
-
-			dbResult.next();
-			count = dbResult.getInt(1);
-
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return count;
+		return questionList;
 	}
 	
 	public Question findById(int id) {
+		Question question = new Question();
 		try {
 			DatabaseConnection database = DatabaseConnection.getInstance();
-
-			String query = "SELECT * FROM question WHERE id = ";
+			String query = "SELECT * FROM question WHERE id = " + id;
 			Statement statement = database.getConnection().createStatement();
 			ResultSet dbResult = statement.executeQuery(query);
-			
 			while (dbResult.next()) {
+				
+				question.setId(dbResult.getInt("id"));
+				question.setTitle(dbResult.getString("title"));
+				question.setRightAnswer(dbResult.getString("right_answer")); 
+				question.setTip(dbResult.getString("tip")); 
 
-			
-				String title = dbResult.getString("title");
-				String wrongAnswers = dbResult.getString("wrong_answers");
-				String rightAnswers = dbResult.getString("right_answer");
-
+				String wrongAnswersFromDb = dbResult.getString("wrong_answers");
+				
+				Set<String> wrongAnswers = new HashSet<>();
+				Collections.addAll(wrongAnswers, wrongAnswersFromDb.split(","));
+				question.setWrongAnswers(wrongAnswers);
 			}
-
-
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		return null;
+		return question;
 	}
 
 	public Question findByRandomId() {
-		return null;
-	}
+		Question question = new Question();
+		try {
+			DatabaseConnection database = DatabaseConnection.getInstance();
+			String query = "SELECT ROUND(RAND() * ( SELECT MAX(id) FROM  table_name)) AS id";
+			Statement statement = database.getConnection().createStatement();
+			ResultSet dbResult = statement.executeQuery(query);
+			while (dbResult.next()) {
+				
+				question.setId(dbResult.getInt("id"));
+				question.setTitle(dbResult.getString("title"));
+				question.setRightAnswer(dbResult.getString("right_answer")); 
+				question.setTip(dbResult.getString("tip")); 
 
+				String wrongAnswersFromDb = dbResult.getString("wrong_answers");
+				
+				Set<String> wrongAnswers = new HashSet<>();
+				Collections.addAll(wrongAnswers, wrongAnswersFromDb.split(","));
+				question.setWrongAnswers(wrongAnswers);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return question;
+	}
 }
